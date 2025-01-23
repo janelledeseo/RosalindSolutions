69698762365477694bc469c6cc21fcdaff9cfa9c
Janelle-Regina Deseo <168034957+janelledeseo@users.noreply.github.com>
2025-01-23 09:42:37 -0800

    Update bioinformatics.py

--- bioinformatics.py
+++ bioinformatics.py
@@ -1,1 +1,10 @@
+### ICA Session 01: 1/23/25
+## For a given 'dna.sequence', write one basic function and calculate GC content of a string
 
+# To count the occurrences of G and C
+g_count = dna_sequence.count('G')
+c_count = dna_sequence.count('C')
+    
+# To calculate the GC content
+gc_content = (g_count + c_count) / len(dna_sequence) * 100
+gc_content
