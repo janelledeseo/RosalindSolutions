4bfbccfbbed700d7cbe703593ca12d3dfa1c802c
Janelle-Regina Deseo <168034957+janelledeseo@users.noreply.github.com>
2025-01-23 10:26:36 -0800

    Update bioinformatics.py
    Updated after in-class review

--- bioinformatics.py
+++ bioinformatics.py
@@ -1,4 +1,5 @@
 ### ICA Session 01: 1/23/25
+## FIRST ATTEMPT
 ## For a given 'dna.sequence', write one basic function and calculate GC content of a string
 
 # To count the occurrences of G and C
@@ -8,3 +9,9 @@ c_count = dna_sequence.count('C')
 # To calculate the GC content
 gc_content = (g_count + c_count) / len(dna_sequence) * 100
 gc_content
+
+### After in-class review:
+## EXAMPLE:
+sequence1 = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
+def gc_content(seq):
+  return (seq.count('G') + seq.count('C'))/len(seq)
