a7a7312c125fc2dd176577ec590a472038427323
Janelle-Regina Deseo <168034957+janelledeseo@users.noreply.github.com>
2025-01-23 19:44:38 -0800

    Update HW-02.py

--- HW-02.py
+++ HW-02.py
@@ -1,1 +1,8 @@
+"""
+Counting DNA Nucleotides
+URL: https://rosalind.info/problems/dna/
+
+Given: A DNA string s of length at most 1000 nt
+Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s
+"""
 
