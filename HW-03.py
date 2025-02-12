8e579427c8cef968e158b10931c9be8a346d6d20
Janelle-Regina Deseo <168034957+janelledeseo@users.noreply.github.com>
2025-02-12 13:38:23 -0800

    Update HW-03.py

--- HW-03.py
+++ HW-03.py
@@ -1,1 +1,9 @@
+# SUBS/PRTM/SPLC/REVP/TRAN/LCSM/ORF/
 
+"""
+Finding a Motif in DNA
+url: http://rosalind.info/problems/subs/
+
+Given: Two DNA strings s and t (each of length at most 1 kbp).
+Return: All locations of t as a substring of s.
+"""
